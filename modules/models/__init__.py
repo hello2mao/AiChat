c99b120db067ca32cc6f030345656780835e5f9b
Hongbin Mao <hello2mao@gmail.com>
2023-07-16 15:38:21 +0800

    Lazy push: 2023年 07月 16日 星期日 15:38:21 CST

---
+++ modules/models/__init__.py
@@ -0,0 +1,4 @@
+from .base import BaseLLM
+from .chatglm.chatglm import ChatGLM
+from .chatgpt.chatgpt import ChatGPT
+from .baichuan.baichuan import Baichuan
---
+++ modules/plugins/__init__.py
@@ -0,0 +1,4 @@
+from .base import BasePlugin
+from .summarization.summarization import Summarization
+from .sql.sql import SQL
+from .ai_friend.ai_friend import AIFriend
